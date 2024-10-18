34ab72853d950cca6d9cbf0d6d965be2dbe53bbb
Viktor Anishchenko <anishchenko.vitya@inbox.ru>
2024-10-18 21:46:05 +0500

    Task1

---
+++ Tyuiu.AnishchenkoVA.Sprint2.Task1.V1.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.AnishchenkoVA.Sprint2.Task1.V1.Lib
+{
+    public class DataService : ISprint2Task1V1
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+
+            res[0] = (a > b) | (b < d);
+            res[1] = (a > b) & (b < a);
+            res[2] = (a > b) || (b < d);
+            res[3] = (a > b) && (b < a);
+            res[4] = !(res[1]);
+            res[5] = (c > d) ^ (c < a);
+
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.AnishchenkoVA.Sprint2.Task1.V1/Program.cs
@@ -0,0 +1,57 @@
+﻿using Tyuiu.AnishchenkoVA.Sprint2.Task1.V1.Lib;
+namespace Tyuiu.AnishchenkoVA.Sprint2.Task1.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Анищенко В. А. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Анищенко Виктор Александрович | ИИПБ-24-2                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность можно чередовать,                                    *");
+            Console.WriteLine("* но использовать один раз в выражении)                                   *");
+            Console.WriteLine("* и логических операций (|, &, ||, &&, !, ^,                              *");
+            Console.WriteLine("* последовательность операций не должна нарушаться), а также              *");
+            Console.WriteLine("* арифметических выражений,                                               *");
+            Console.WriteLine("* которая вернет логическую последовательность(массив):                   *");
+            Console.WriteLine("* (True, False, True, False, True, False),                                 *");
+            Console.WriteLine("* при a = 119, b = 196, c = 134, d = 327                                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int a = 119;
+            Console.WriteLine("a = " + a);
+
+            int b = 196;
+            Console.WriteLine("a = " + b);
+
+            int c = 134;
+            Console.WriteLine("a = " + c);
+
+            int d = 324;
+            Console.WriteLine("a = " + d);
+
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+        }
+    }
+}
