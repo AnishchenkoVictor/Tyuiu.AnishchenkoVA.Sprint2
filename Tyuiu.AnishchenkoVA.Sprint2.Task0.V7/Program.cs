9eb6eff78148931bcc68b84cfd814ac312805e22
Viktor Anishchenko <anishchenko.vitya@inbox.ru>
2024-10-18 20:50:42 +0500

    Add project files.

---
+++ Tyuiu.AnishchenkoVA.Sprint2.Task0.V7/Program.cs
@@ -0,0 +1,49 @@
+﻿using Tyuiu.AnishchenkoVA.Sprint2.Task0.V7.Lib;
+namespace Tyuiu.AnishchenkoVA.Sprint2.Task0.V7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Анищенко В. А. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #2                                                               *");
+            Console.WriteLine("* Тема: Операции сравнения                                                *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #7                                                              *");
+            Console.WriteLine("* Выполнил: Анищенко Виктор Александрович | ИИПБ-24-2                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу из операций сравнений (==, !=, <, >, <=, >=,         *");
+            Console.WriteLine("* последовательность операций не должна нарушаться)                       *");
+            Console.WriteLine("* и арифметических выражений,                                             *");
+            Console.WriteLine("* которая вернет логическую последовательность(массив):                   *");
+            Console.WriteLine("* (True, False, True, True, True, False), при x = 103, y = 475            *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int x = 103;
+
+            Console.WriteLine("X = " + x);
+
+            int y = 475;
+
+            Console.WriteLine("Y = " + y);
+
+            bool[] res = new bool[6];
+            res = ds.GetCompareOperations(x, y);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+        }
+    }
+}
